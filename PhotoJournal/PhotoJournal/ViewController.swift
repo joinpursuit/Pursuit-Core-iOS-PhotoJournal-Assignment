ca6d52fc4bf95d64a24e517715ba372110c554f5
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-02 12:44:41 -0400

    initial commit

---
+++ PhotoJournal/PhotoJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournal
+//
+//  Created by Sunni Tang on 10/2/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
