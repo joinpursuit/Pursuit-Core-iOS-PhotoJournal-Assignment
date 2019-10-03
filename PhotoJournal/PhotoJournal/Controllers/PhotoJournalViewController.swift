2b574bb67ec8f67719022c887c48490ac01f62d1
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-03 16:15:54 -0400

    adds storyboard objects and creates files for view controllers

--- PhotoJournal/PhotoJournal/Controllers/PhotoJournalViewController.swift
+++ PhotoJournal/PhotoJournal/Controllers/PhotoJournalViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PhotoJournalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ PhotoJournal/PhotoJournal/Controllers/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  PhotoJournal
+//
+//  Created by Sunni Tang on 10/3/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
