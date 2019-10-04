ea0776c2757cc68319ae0e07e527fd804e757c90
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro.local>
2019-10-04 17:36:42 -0400

    Added PersistenceHelper

---
+++ Unit4Assignment1Proj/Unit4Assignment1Proj/PersistenceHelper.swift
@@ -0,0 +1,45 @@
+//
+//  PersistenceHelper.swift
+//  Unit4Assignment1Proj
+//
+//  Created by Kevin Natera on 10/4/19.
+//  Copyright © 2019 Kevin Natera. All rights reserved.
+//
+
+import Foundation
+struct PersistenceHelper<T: Codable> {
+    
+    func getObjects() throws -> [T] {
+        guard let data = FileManager.default.contents(atPath: url.path) else {
+            return []
+        }
+        return try PropertyListDecoder().decode([T].self, from: data)
+    }
+    
+    func save(newElement: T) throws {
+        var elements = try getObjects()
+        elements.append(newElement)
+        try replace(elements: elements)
+    }
+    
+    func replace(elements: [T]) throws {
+      let serializedData = try PropertyListEncoder().encode(elements)
+      try serializedData.write(to: url, options: Data.WritingOptions.atomic)
+    }
+    
+    init(fileName: String){
+        self.fileName = fileName
+    }
+    
+    private let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
+    private func filePathFromDocumentsDirectory(name: String) -> URL {
+        return documentsDirectory.appendingPathComponent(name)
+    }
+    
+    private let fileName: String
+    
+    private var url: URL {
+        return filePathFromDocumentsDirectory(name: fileName)
+    }
+}
