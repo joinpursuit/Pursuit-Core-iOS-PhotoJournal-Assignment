f8da151b6f1d4c2851aad460d7fa75d703e5bfc2
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro.local>
2019-10-06 21:22:56 -0400

    Created Protocol.swift

---
+++ Unit4Assignment1Proj/Unit4Assignment1Proj/Helpers/Protocols.swift
@@ -0,0 +1,15 @@
+//
+//  Protocols.swift
+//  Unit4Assignment1Proj
+//
+//  Created by Kevin Natera on 10/6/19.
+//  Copyright © 2019 Kevin Natera. All rights reserved.
+//
+
+import Foundation
+
+
+protocol PhotoCellDelegate: AnyObject {
+    func showActionSheet(tag: Int)
+}
+
