245acfe0af6d4fb00df4b30a3df810888ed1c1ea
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro.local>
2019-10-06 23:12:37 -0400

    Created AddPhotoViewController

--- Unit4Assignment1Proj/Unit4Assignment1Proj/Views/AddPhotoViewController.swift
+++ Unit4Assignment1Proj/Unit4Assignment1Proj/Views/AddPhotoViewController.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2019 Kevin Natera. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AddPhotoViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var titleTextView: UITextView!
+    @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var cancelButtonOutlet: UIBarButtonItem!
+    @IBOutlet weak var saveButtonOutlet: UIBarButtonItem!
+    @IBOutlet weak var photoLibraryButtonOutlet: UIBarButtonItem!
+    @IBOutlet weak var camerabuttonOutlet: UIBarButtonItem!
+    
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func photoLibraryButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
